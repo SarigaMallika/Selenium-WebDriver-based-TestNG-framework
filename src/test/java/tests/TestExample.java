2da688458dd8fcb1d5d370d056f06a9ea5b2e9d3
Sariga Sajeev <sariga947@gmail.com>
2024-01-01 11:09:33 +0530

    1. **Initialize TestNG Framework**
       - Create project structure
       - Set up Maven for dependency management
       - Add Selenium WebDriver dependencies
       - Configure Log4j for logging
       - Create a basic TestNG class (TestExample) for testing setup
    
       2. **Add Data-Driven Testing Feature**
    
          - Implement data provider method for parameterized tests
          - Extend TestExample to use data-driven testing for Google search
          - Update README with usage instructions for data-driven testing

--- src/test/java/tests/TestExample.java
+++ src/test/java/tests/TestExample.java
@@ -15,7 +15,6 @@ public class TestExample {
     WebDriver driver;
     private static final Logger logger = LogManager.getLogger(TestExample.class);
 
-
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
